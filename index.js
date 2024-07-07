01fd07dc044873e4c5033428f4835c796250118d
Abhishek Singh <127656851+absingh0103@users.noreply.github.com>
2024-07-07 14:44:46 +0530

    bug fix

--- index.js
+++ index.js
@@ -151,49 +151,7 @@ server.post("/create-payment-intent", async (req, res) => {
 
 // ! Stripe Payment Integration End
 
-// WebHook For Stripe
-// Webhook is used to tell server that the payment of particular order is completed or not in case of any system failure
-// webhook directly communicate to server not to froentend
-
-// TODO:Here We will Capture Actual Order And then find whether payment succeed or not
-
-// This is your Stripe CLI webhook secret for testing your endpoint locally.
-// const endpointSecret = process.env.WEBHOOK_ENDPOINT_SECRET;
-
-// server.post(
-//   "/webhook",
-//   express.raw({ type: "application/json" }),
-//   (request, response) => {
-//     const sig = request.headers["stripe-signature"];
-
-//     let event;
-
-//     try {
-//       event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-//     } catch (err) {
-//       response.status(400).send(`Webhook Error: ${err.message}`);
-//       return;
-//     }
-//     // Handle the event
-//     switch (event.type) {
-//       case "payment_intent.succeeded":
-//         const paymentIntentSucceeded = event.data.object;
-//         // Then define and call a function to handle the event payment_intent.succeeded
-//         break;
-//       // ... handle other event types
-//       default:
-//         console.log(`Unhandled event type ${event.type}`);
-//     }
-//     // Return a 200 response to acknowledge receipt of the event
-//     response.send();
-//   }
-// );
-
-
 // Routes Middleware
-
-
-
 
 server.use("/products", productsRouter);
 server.use("/brands", brandsRouter);
